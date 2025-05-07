db31d937660b70fea2d0ef980e7e9637cc2c9d6e
a8282530 <58839907+a8282530@users.noreply.github.com>
2025-05-07 20:49:13 +0800

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -1,5 +1,5 @@
 document.addEventListener('alpine:init', () => {
-    const host = 'https://ball-v1.duole.lol';
+    const host = 'https://test-test-vmhappqoeo.cn-hangzhou.fcapp.run';
     const voices = window.speechSynthesis.getVoices();
     const Toast = Swal.mixin({
         toast: true,
